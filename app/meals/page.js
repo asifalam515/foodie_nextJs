244d0d4fcf1e03ba57255aa6a9034853a86cf09e
asifalam515 <asibulalam515@gmail.com>
2024-12-21 20:32:57 +0600

    added

--- app/meals/page.js
+++ app/meals/page.js
@@ -16,10 +16,25 @@ export default function Home() {
             <Link href="/community">Join the Community</Link>
             <Link href="/meals">Explore Meals</Link>
             {/* sweeet alert */}
-            Swal.fire({
-  title: "Good job!",
-  text: "You clicked the button!",
-  icon: "success"
+            al.fire({
+  title: "<strong>HTML <u>example</u></strong>",
+  icon: "info",
+  html: `
+    You can use <b>bold text</b>,
+    <a href="#" autofocus>links</a>,
+    and other HTML tags
+  `,
+  showCloseButton: true,
+  showCancelButton: true,
+  focusConfirm: false,
+  confirmButtonText: `
+    <i class="fa fa-thumbs-up"></i> Great!
+  `,
+  confirmButtonAriaLabel: "Thumbs up, great!",
+  cancelButtonText: `
+    <i class="fa fa-thumbs-down"></i>
+  `,
+  cancelButtonAriaLabel: "Thumbs down"
 });
           </div>
         </div>
